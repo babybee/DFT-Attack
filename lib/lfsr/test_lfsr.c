f7d007c100d66391de848b3e9042719d907e39c9
Bo Zhu <zhubo03@gmail.com>
2011-07-23 11:56:39 -0400

    added a simple test for lfsr module

--- lib/lfsr/test_lfsr.c
+++ lib/lfsr/test_lfsr.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 #include "lfsr.h"
 
+#define ONE 	1
+#define ZERO 	0
+
+#define LEN	2
+
 int main()
 {
+	unsigned char state[LEN] = {ONE, ONE};
+
+	unsigned char feedback[LEN] = {1, 1};
+
+	int i;
+	
+	for (i = 0; i < (2 << (LEN - 1)) - 1; i++) {
+		printf("%2x ", state[0]);
+		lfsr(state, state, feedback, LEN);
+	}
+	printf("\n");
+	for (i = 0; i < (2 << (LEN - 1)) - 1; i++) {
+		printf("%2x ", state[0]);
+		lfsr(state, state, feedback, LEN);
+	}
+	printf("\n");
+		
 	return 0;
 }
