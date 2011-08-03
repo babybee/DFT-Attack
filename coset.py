c0ac1dab962360c7996b702a847d032ed5146080
Bo Zhu <zhubo03@gmail.com>
2011-08-03 12:11:21 -0400

    ...

--- coset.py
+++ coset.py
@@ -2,12 +2,22 @@
 
 def coset(N):
     "return a dict with coset leaders and its cosets size"
-    result = dict()
+    result = {}
 
-    flags = [False] * N
+    flag = [False] * N
     for x in range(N):
-        if flags[x]: # if already processed
+        if flag[x]: # if already processed
             continue
 
         size = 1
-        while x
+        while (x * (2 ** size)) % N != x:
+            flag[(x * (2 ** size)) % N] = True
+            size += 1
+
+        result[x] = size
+
+    return result
+
+
+if __name__ == '__main__':
+    print coset(15)
--- dft.py
+++ dft.py
@@ -14,6 +14,7 @@ def dft(sequence):
     assert 2 * L <= N
     # should have some backup procedure?
 
+    # selection of parameters
     # fine smallest n
     n = 1
     while 0 == (2 ** n - 1) % n:
@@ -28,10 +29,33 @@ def dft(sequence):
     seq_b_iv = []
     for i in range(n):
         seq_b_iv.append((beta ** i).trace())
-    seq_b = lfsr(fun_f, seq_b_iv)
+    seq_b_generator = lfsr(fun_f, seq_b_iv)
 
     # let alpha be an element in GF(2^n) with order N
     alpha = beta ** ((2 ** n - 1) / N)
+
+    # procedure
+    # step 1. compute coset
+    I = coset(N)
+
+    # step 2. 
+    fun_p_extended = TheExtensionField(fun_p)
+
+    spectra_A = {}
+    for k in I:
+        if fun_p_extended(alpha ** k) != 0:
+            spectra_A[k] = 0
+            continue
+
+        # sub-routine for computing A_k
+        # 1. get coset size
+        m = I[k]
+
+
+
+
+
+
 
 
 
