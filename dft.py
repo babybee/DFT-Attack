3598b665dc9c1f5320ea6ca18e5914e88f945bda
Bo Zhu <zhubo03@gmail.com>
2011-08-08 12:42:36 -0400

    go for lunch

--- dft.py
+++ dft.py
@@ -19,7 +19,9 @@ def dft(seq_a):
     N = len(seq_a)
     fun_p = bma(seq_a) # bma() will change the input as periodic
     L = fun_p.degree()
-    
+    # print fun_p
+    # print L
+
     # treat the input seq as peroidic so remove this
     # assert 2 * L <= N
 
@@ -28,12 +30,14 @@ def dft(seq_a):
     n = 1
     while 0 != (2 ** n - 1) % N:
         n += 1
+    # print n
 
     # define the extension field
     TheExtensionField = GF(2 ** n, 'beta')
     beta = TheExtensionField.gen()
     fun_f = TheExtensionField.polynomial()
     TheExtensionPolynomialRing = TheExtensionField['Y']
+    # print fun_f
 
     # construct a new m-seq {b_t}
     seq_b_iv = []
@@ -43,16 +47,21 @@ def dft(seq_a):
     seq_b = []
     for i in range(2 ** len(seq_b_iv) - 1):
         seq_b.append(seq_b_generator.next())
+    # print seq_b_iv
+    # print seq_b
 
     # let alpha be an element in GF(2^n) with order N
     alpha = beta ** ((2 ** n - 1) / N)
+    # print alpha, alpha ** N
 
     # procedure
     # step 1. compute coset
     I = coset(N)
+    # print I
 
     # step 2. 
     fun_p_extended = TheExtensionPolynomialRing(fun_p)
+    # print fun_p_extended
 
     spectra_A = {}
     for k in I:
@@ -60,12 +69,12 @@ def dft(seq_a):
             spectra_A[k] = 0
             continue
 
-        print fun_p_extended(alpha ** k)
-        print fun_p_extended
+        # print 'k', k, fun_p_extended(alpha ** k)
 
         # sub-routine for computing A_k
         # 1. get coset size
         m = I[k]
+        # print k, m, n
 
         # 2. k-decimation sequence
         seq_c = []
@@ -77,22 +86,24 @@ def dft(seq_a):
                 seq_c.append(Tr_m(alpha ** (k * t), m))
         else:
             import sys
-            sys.stderr.write("should never happen?")
+            sys.stderr.write("should never happen?\n")
             sys.exit(-1)
 
+        # print seq_b
+        # print seq_c
+
         fun_p_k = bma(seq_c)
+        print fun_p_k
 
         matrix_M_ele = []
         for i in range(m):
             for ele in range(i, m + i):
                 matrix_M_ele.append(seq_c[ele])
         matrix_M = matrix(GF(2), m, m, matrix_M_ele)
+        # print matrix_M
 
         # 3. contruct a filter
         fun_q = fun_p / fun_p_k
-        print fun_p
-        print fun_p_k
-        print fun_q
 
         # 4. compute the time convolution
         seq_v_generator = convolution(seq_a, fun_q)
